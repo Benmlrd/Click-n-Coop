1792a48a815676de09037c7769197ba84931e478
Nathan715 <nathan.mahe2001@gmail.com>
2021-03-30 16:57:46 +0200

    Modifications mineures apportées au layout du menu principal (couleurs) e création du layout du jeu Rythm.

---
+++ app/src/main/java/com/example/clickandcoop/Rythm.java
@@ -0,0 +1,34 @@
+package com.example.clickandcoop;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class Rythm extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.menu_principal, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.b_mash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Rythm.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+}
